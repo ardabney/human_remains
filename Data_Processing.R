dc4fd6741ccdae66b113150b53636a745521051f
tessa-johnson <johnson02@gmail.com>
2017-05-30 09:42:51 -0500

    Add files via upload

---
+++ Data_Processing.R
@@ -0,0 +1,26 @@
+# Change Based on User's Preference 
+setwd("~/Desktop")
+
+#load in needed libraries
+library(phyloseq)
+
+#import data
+std_dta  <- import_biom("f120_r1k.biom") #whole data
+m_dta <- data.frame(sample_data(std_dta)) #meta data
+OTU_dta <- data.frame(otu_table(std_dta)) #OTU data
+t_dta <- data.frame(tax_table(std_dta)) #Taxa data
+
+# Data Processing - Meta
+mt_dta<-m_dta[!duplicated(m_dta$Pack_ID),] #patients
+row.names(mt_dta)<-mt_dta$Pack_ID
+meta_dta <-subset(mt_dta,select=c("Estimated_PMI","Race","Manner.of.Death","Season","Sex","Weight_Status","Event_Location","BMI_Group","Age_Group"))
+
+# Data Processing - Taxa
+colnames(t_dta) <- c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+t_dta$Kingdom <- gsub("k__", "", t_dta$Kingdom)
+t_dta$Phylum <- gsub("p__", "", t_dta$Phylum)
+t_dta$Class <- gsub("c__", "", t_dta$Class)
+t_dta$Order <- gsub("o__", "", t_dta$Order)
+t_dta$Family <- gsub("f__", "", t_dta$Family)
+t_dta$Genus <- gsub("g__", "", t_dta$Genus)
+t_dta$Species <- gsub("s__", "", t_dta$Species)
