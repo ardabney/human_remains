e7ee69897bf72c24cd45bfc3f5a4054e77245008
tessa-johnson <johnson02@gmail.com>
2017-06-11 12:16:31 -0500

    New Edits Start on Line 80

--- Tessa_Models.R
+++ Tessa_Models.R
@@ -53,7 +53,7 @@ select_otu <- otu_dta[,select]
 
 #Creating Test and Training Data
 set.seed(101)
-train<-sample(1:120,round(120*0.66))
+train<-sample(1:120,80)
 train_mt<-meta_dta[train,]
 train_otu<-select_otu[train,]
 test_mt <- meta_dta[-train,]
@@ -64,5 +64,63 @@ training <- data.frame(train_mt,train_otu)
 pmi_mult <- multinom(Estimated_PMI ~ ., data = training, MaxNWts=1500)
 testing <- data.frame(test_mt,test_otu)
 pred_pmi <- predict(pmi_mult, newdata = testing)
-table(pred_pmi, testing$Estimated_PMI)
+table(pred_pmi, testing$Estimated_PMI) 
 confusionMatrix(pred_pmi, testing$Estimated_PMI)
+
+#Cross Validation: 
+
+#Testing & Training Sets
+set.seed(101)
+train<-sample(1:120,80)
+train_mt<-meta_dta[train,]
+train_otu<-  otu_dta[train,]
+test_mt <- meta_dta[-train,]
+test_otu <- otu_dta[-train,]
+
+# We'll use 8-fold CV since 80 divides nicely into 8 folds
+
+# Divide Samples into 8 folds
+new_rand_order <-sample(1:80,80)
+fold_1 <- new_rand_order[1:10]
+fold_2 <- new_rand_order[11:20]
+fold_3 <- new_rand_order[21:30]
+fold_4 <- new_rand_order[31:40]
+fold_5 <- new_rand_order[41:50]
+fold_6 <- new_rand_order[51:60]
+fold_7 <- new_rand_order[61:70]
+fold_8 <- new_rand_order[71:80]
+fold_samples = list(fold_1, fold_2, fold_3, fold_4, fold_5, fold_6, fold_7, fold_8)
+
+# Cross Validation
+f_sizes = c(seq(10, 926, by = 10), 926)
+acc_f_b = matrix(NA, nrow = length(f_sizes), ncol = 8) # Misclassification Rate
+for(b in 1:8) {
+  cat(".")
+  MT_test = train_mt[fold_samples[[b]],]
+  OTU_test = train_otu[fold_samples[[b]],] 
+  MT_train = train_mt[-fold_samples[[b]],]
+  OTU_train = train_otu[-fold_samples[[b]],] 
+  
+  ## Feature selection.
+p_val_otu <- NULL
+for(i in 1:926){
+    selected <- data.frame(MT_train,OTU_train[,i])
+    pmi_fit <- multinom(Estimated_PMI ~ ., selected, model = TRUE)
+    test_fit <- Anova(pmi_fit)
+    p_val_otu[i] <- test_fit[9,3]
+}
+oo = order(p_val_otu)
+for(f in 1:length(f_sizes)) {
+    feature_set = oo[1:f_sizes[f]]
+    otu_train = OTU_train[,feature_set]
+    otu_test = OTU_test[,feature_set]
+    dta_test = data.frame(MT_test,otu_test)
+    dta_train = data.frame(MT_train,otu_train)
+    pmi_mult <- multinom(Estimated_PMI ~ ., data = dta_train, MaxNWts=15000)
+    pred_pmi <- predict(pmi_mult, newdata = dta_test)
+    c_matr <- confusionMatrix(pred_pmi, dta_test$Estimated_PMI)
+    acc_f_b[f, b] = 1 - c_matr$overall[1]
+    
+  }
+}
+acc_f = rowMeans(acc_f_b)
